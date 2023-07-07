bc30696cba08943a5db954ad9450ef925f09ba4a
arthurvictory <98421766+arthurvictory@users.noreply.github.com>
2023-07-07 15:33:39 -0500

    first commit

---
+++ refactor.py
@@ -0,0 +1,14 @@
+# 1 Convert a number to String
+
+def number_to_string(num):
+    return str(num)
+
+
+# 2 Remove first and last character from a string
+
+def remove_char(s):
+    return s[1:-1]
+
+# 3 Make uppercase
+def make_upper_case(s):
+    return s.upper()
