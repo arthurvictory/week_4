bf4adb47aac2ff364b58eff1ba23db3165d6bf04
arthurvictory <98421766+arthurvictory@users.noreply.github.com>
2023-07-07 15:42:22 -0500

    added kata links

--- refactor.py
+++ refactor.py
@@ -1,14 +1,16 @@
 # 1 Convert a number to String
-
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8
 def number_to_string(num):
     return str(num)
 
 
 # 2 Remove first and last character from a string
+# https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
 
 def remove_char(s):
     return s[1:-1]
 
 # 3 Make uppercase
+# https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
 def make_upper_case(s):
     return s.upper()
